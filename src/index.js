18cef661de8f64d67959e08c91e3ec1e4d5d0db4
Oleksandr Mezhenskyi <oleksandr.mezhenskyi@chisw.com>
2021-08-29 20:09:58 +0300

    feat: implement function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = [];
+  const BRACKETS_PAIR = {};
+
+  for (let i = 0; i < bracketsConfig.length; i++) {
+    for (let j = 0; j < bracketsConfig[i].length; j++) {
+      if (j % 2 === 0) {
+        OPEN_BRACKETS.push(bracketsConfig[i][j]);
+      } else {
+        BRACKETS_PAIR[bracketsConfig[i][j]] = bracketsConfig[i][j - 1];
+      }
+    }
+  }
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let currentSymbol = str[i];
+
+    if (OPEN_BRACKETS.includes(currentSymbol)) {
+      stack.push(currentSymbol);
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+
+      let topElement = stack[stack.length - 1];
+
+      if (BRACKETS_PAIR[currentSymbol] === topElement) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+
+  return stack.length === 0;
 }
